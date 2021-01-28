e818b0aff88f792af01ee24148db47432a304950
Carlos Lopez <krlosaren@gmail.com>
2021-01-28 19:05:18 -0300

    [add] config files and start project

---
+++ src/routes/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// styles
+import '../styles/components/index.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
