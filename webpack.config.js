6285df9b927ee2388fe341a563ac5190f63a6bba
Carlos Lopez <krlosaren@gmail.com>
2021-02-01 22:30:55 -0300

    [add] optimization config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,6 +15,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/',
+    chunkFilename: '[id].[chunkhash].js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -57,16 +64,28 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DllReferencePlugin({
+      manifest: require('./modules-manifest.json'),
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
+      outputPath: 'js',
+      publicPath: 'http://localhost:4000/js',
+    }),
+    new WebpackManifestPlugin(),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/bundle.*'],
+    }),
     new CopyWebpackPlugin({
-      patterns: [{ from: './src/styles/assets/favicon-01.ico', to: './dist' }],
+      patterns: [{ from: './src/styles/assets/favicon-01.ico', to: 'assets' }],
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
-      favicon: './src/styles/assets/favicon-01.ico',
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
+      chunkFilename: 'styles/[id].[hash].css',
     }),
   ],
   devServer: {
@@ -74,5 +93,9 @@ module.exports = {
     compress: true,
     port: 4000,
     historyApiFallback: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
   },
 };
