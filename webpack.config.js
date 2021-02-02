c667c17bc8a2834ad2d57f013ac4ff1b5e9764c2
Carlos Lopez <krlosaren@gmail.com>
2021-02-01 21:38:33 -0300

    [fix] problems with changes in master break all

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -56,9 +57,13 @@ module.exports = {
     ],
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/styles/assets/favicon-01.ico', to: './dist' }],
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
+      favicon: './src/styles/assets/favicon-01.ico',
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
